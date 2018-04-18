82167ae668276fc57410746a6bf3dcb47a61dc2d
M. Salah <eng.msalah.abdullah@gmail.com>
2018-04-18 09:58:26 +0400

    Update sample 2

--- sparx/js/html5-scripts.js
+++ sparx/js/html5-scripts.js
@@ -140,15 +140,16 @@ $(function() {
               });
           $('#year').text(austDay.getFullYear());
      });
+	 
 
 /***************************************************
 	ROTATING INTRO TEXT SCRIPT
 ***************************************************/	
 !function($){var defaults={animation:"dissolve",separator:",",speed:2000};$.fx.step.textShadowBlur=function(fx){$(fx.elem).prop("textShadowBlur",fx.now)
 .css({textShadow:"0 0 "+Math.floor(fx.now)+"px black"})};$.fn.textrotator=function(options){var settings=$.extend({},defaults,options);
-return this.each(function(){var el=$(this);var array=[];$.each(el.text().split(settings.separator),function(key,value){array.push(value)});el.text(array[0]);
-var rotate=function(){switch(settings.animation){case"dissolve":el.animate({textShadowBlur:0,opacity:0},500,function(){index=$.inArray(el.text(),array);
-if((index+1)==array.length){index=-1}el.text(array[index+1]).animate({textShadowBlur:0,opacity:1},500)});
-;break;case"fade":el.fadeOut(settings.speed,function(){index=$.inArray(el.text(),
-array);if((index+1)==array.length){index=-1}el.text(array[index+1]).fadeIn(settings.speed)});break}};setInterval(rotate,settings.speed*2)})}}
+return this.each(function(){var el=$(this);var array=[];$.each(el.html().split(settings.separator),function(key,value){array.push(value)});el.html(array[0]);
+var rotate=function(){switch(settings.animation){case"dissolve":el.animate({textShadowBlur:0,opacity:0},500,function(){index=$.inArray(el.html(),array);
+if((index+1)==array.length){index=-1}el.html(array[index+1]).animate({textShadowBlur:0,opacity:1},500)});
+;break;case"fade":el.fadeOut(settings.speed,function(){index=$.inArray(el.html(),
+array);if((index+1)==array.length){index=-1}el.html(array[index+1]).fadeIn(settings.speed)});break}};setInterval(rotate,settings.speed*2)})}}
 (window.jQuery);
